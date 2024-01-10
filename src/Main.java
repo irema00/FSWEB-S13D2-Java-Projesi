6aad5f5c923fb222d9cb8a9a43e8e1ce25b5d152
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-10 23:54:44 +0300

    adds isPalindrome method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1-finding palindrome");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
     }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        String num = String.valueOf(number);
+        StringBuilder reversed = new StringBuilder();
+        for (int i = num.length() - 1; i >= 0; i--) {
+            reversed.append(num.charAt(i));
+        }
+        return num.equals(String.valueOf(reversed));
+    }
+
 }
